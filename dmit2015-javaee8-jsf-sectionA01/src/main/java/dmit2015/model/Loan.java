6e75e5d54c04ca36c30597947b3bdadb5bcd7cb2
Sam Wu <swu@nait.ca>
2019-02-11 16:06:20 -0700

    Added JSF example on Loan and LoanSchedule

---
+++ dmit2015-javaee8-jsf-sectionA01/src/main/java/dmit2015/model/Loan.java
@@ -0,0 +1,84 @@
+package dmit2015.model;
+
+import org.apache.commons.math3.util.Precision;
+
+public class Loan {
+	
+	private double mortgageAmount = 100000;
+	private double annualInterestRate = 5;
+	private int amortizationPeriod = 25;
+	
+	public double getMortgageAmount() {
+		return mortgageAmount;
+	}
+	public void setMortgageAmount(double mortgageAmount) {
+		this.mortgageAmount = mortgageAmount;
+	}
+	public double getAnnualInterestRate() {
+		return annualInterestRate;
+	}
+	public void setAnnualInterestRate(double annualInterestRate) {
+		this.annualInterestRate = annualInterestRate;
+	}
+	public int getAmortizationPeriod() {
+		return amortizationPeriod;
+	}
+	public void setAmortizationPeriod(int amortizationPeriod) {
+		this.amortizationPeriod = amortizationPeriod;
+	}
+	
+	public Loan() {
+
+	}
+	
+	public Loan(double mortgageAmount, double annualInterestRate, int amortizationPeriod) {
+		this.mortgageAmount = mortgageAmount;
+		this.annualInterestRate = annualInterestRate;
+		this.amortizationPeriod = amortizationPeriod;
+	}
+	
+	public double getMonthlyPayment() {
+		double monthyPayment = 0;
+		monthyPayment = mortgageAmount * (Math.pow(1 + annualInterestRate/200, 1.0/6) - 1)
+				/ (1 - Math.pow(1 + annualInterestRate/200, -12 * amortizationPeriod / 6.0));
+		monthyPayment = Precision.round(monthyPayment, 2);
+		return monthyPayment;
+	}
+	
+	public LoanSchedule[] getLoanSchedule() {
+		LoanSchedule[] loanScheduleTable = null;
+		int numberOfPayments = amortizationPeriod * 12;
+		loanScheduleTable = new LoanSchedule[numberOfPayments];
+		double remainingBalance = mortgageAmount;
+		double principalPaid = 0;
+		double interestPaid = 0;
+		double monthlyPercentageRate = Math.pow(1 + annualInterestRate / 200, 1.0/6.0 ) - 1;
+		double monthlyPayment = getMonthlyPayment();
+		for (int paymentNumber = 1; paymentNumber <= numberOfPayments; paymentNumber++) {
+			int index = paymentNumber - 1;
+			interestPaid = monthlyPercentageRate * remainingBalance;
+			interestPaid = Precision.round(interestPaid, 2);
+			
+			if (remainingBalance < monthlyPayment) {
+				principalPaid = remainingBalance;
+			} else {
+				principalPaid = monthlyPayment - interestPaid;
+				principalPaid = Precision.round(principalPaid, 2);
+			}
+			
+			remainingBalance = remainingBalance - principalPaid;
+			remainingBalance = Precision.round(remainingBalance,2 );				
+			
+			LoanSchedule singleLoanSchedule = new LoanSchedule();
+			singleLoanSchedule.setPaymentNumber(paymentNumber);
+			singleLoanSchedule.setInterestPaid(interestPaid);
+			singleLoanSchedule.setPrincipalPaid(principalPaid);
+			singleLoanSchedule.setRemainingBalance(remainingBalance);
+			loanScheduleTable[index] = singleLoanSchedule;
+		}
+		
+		return loanScheduleTable;
+	}
+	
+	
+}
